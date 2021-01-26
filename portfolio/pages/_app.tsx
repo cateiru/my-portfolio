e3577a8a61a198626c92741c47ad39424993ea79
Yuto Watanabe <yuto.w51942@gmail.com>
2021-01-26 10:32:51 +0900

    rootにoverflowの設定を追加

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -5,9 +5,19 @@ import '../styles/globals.css'
 import PropTypes from 'prop-types';
 import PageHead from '../components/PageHead'
 import { CookiesProvider } from 'react-cookie'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import * as React from 'react'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      overflow: 'hidden',
+    }
+  })
+)
+
 export default function App({Component, pageProps}) {
+  const classes = useStyles()
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -17,12 +27,12 @@ export default function App({Component, pageProps}) {
   }, []);
 
   return (
-  <React.Fragment>
+  <div className={classes.root}>
     <CookiesProvider>
         <PageHead />
         <Component {...pageProps} />
     </CookiesProvider>
-  </React.Fragment>
+  </div>
   )
 }
 
