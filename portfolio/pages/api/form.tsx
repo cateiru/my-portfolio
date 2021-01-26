347a95ad9238973bfef9d6c67b911336e52de8b1
Yuto Watanabe <yuto.w51942@gmail.com>
2021-01-26 20:37:23 +0900

    axios to node-fetch

--- portfolio/pages/api/form.tsx
+++ portfolio/pages/api/form.tsx
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import axios from 'axios'
-
+import fetch from 'node-fetch'
 
 interface Text {
   name: string,
@@ -37,22 +37,29 @@ ${data.text}
   console.log(text)
 
   const config = {
-    headers: {
       'Accept': 'application/json',
       'Content-type': 'application/json',
-    },
   }
 
   const postData = {
     content: text
   }
 
-  axios.post(token, postData, config)
-    .then(result => {
-      console.log(result)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+  // axios.post(token, postData, config)
+  //   .then(result => {
+  //     console.log(result)
+  //   })
+  //   .catch(error => {
+  //     console.log(error)
+  //   })
+
+  fetch(token, {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: config,
+  })
+  .then(res => {
+    console.log(res)
+  })
 }
 
