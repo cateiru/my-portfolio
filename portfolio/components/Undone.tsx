66e43c34e5ca34a162eac53d306b717adc22f210
Yuto Watanabe <yuto.w51942@gmail.com>
2021-01-28 03:20:23 +0900

    未完成ダイアログを消せるように変更、下に移動

--- portfolio/components/Undone.tsx
+++ portfolio/components/Undone.tsx
@@ -1,9 +1,12 @@
+import React from 'react'
 import Snackbar from '@material-ui/core/Snackbar'
 import MuiAlert from '@material-ui/lab/Alert'
 
 export default function Undone() {
+  const [isAlert, setIsAlert] = React.useState(true)
+
   return (
-    <Snackbar open={true} anchorOrigin={{ vertical: 'top', horizontal: 'center'}} >
+    <Snackbar open={isAlert} anchorOrigin={{ vertical: 'bottom', horizontal: 'center'}} onClose={() => {setIsAlert(false)}} >
       <MuiAlert severity="warning" variant="filled">
         このページは現在、作成中です。
       </MuiAlert>
