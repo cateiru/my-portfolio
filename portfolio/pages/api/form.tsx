6a57003423c504cf2e0102ad1414f97e1b7559fa
Yuto Watanabe <yuto.w51942@gmail.com>
2021-01-28 03:20:39 +0900

    フォームでipアドレスも取得する

--- portfolio/pages/api/form.tsx
+++ portfolio/pages/api/form.tsx
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import axios, { AxiosRequestConfig } from 'axios'
 import qs from 'querystring'
+import { isIP } from "net"
 
 interface Text {
   name: string,
@@ -14,7 +15,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   let status = 400
   if (req.method === 'POST'){
     const getData: Text = req.body
-    status = await sendDiscord(getData)
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    status = await sendDiscord(getData, ip)
   }
 
   res.setHeader("content-type", "application/json")
@@ -22,14 +24,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   res.end()
 }
 
-async function sendDiscord(data: Text): Promise<number>{
+async function sendDiscord(data: Text, ip: string[] | string): Promise<number>{
   const token = process.env.DISCORD_TOKEN
+  if(typeof ip !== 'string'){
+    ip = ip.join(' ')
+  }
 
   const text = `【新着問い合わせ】
 * お名前: ${data.name}
 * メールアドレス: ${data.mail}
 * 送信日時: ${data.date}
-* 内容
+* IPアドレス: ${ip}
+------
+${data.title}
+
 ${data.text}
   `
 
